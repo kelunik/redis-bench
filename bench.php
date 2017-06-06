cb7e065f6e840bf020d8e26ea3baa83640a93c3e
Niklas Keller <me@kelunik.com>
2017-06-06 17:11:16 +0200

    Update to newer parsers

--- bench.php
+++ bench.php
@@ -1,16 +1,18 @@
 <?php
 
 use Clue\Redis\Protocol\Parser\ResponseParser;
+use Kelunik\StreamingResp\IterativeGeneratorRespParser;
 use Kelunik\StreamingResp\IterativeRespParser;
-use Kelunik\StreamingResp\RecursiveRespParser;
+use Kelunik\StreamingResp\RecursiveGeneratorRespParser;
+use Kelunik\StreamingResp\RecursiveGeneratorRespParserWithoutInlining;
 
 chdir(__DIR__);
 error_reporting(E_ALL);
 
 require "vendor/autoload.php";
 
-function bench(BenchCase $benchCase) {
-    foreach (["simpleString", "bulkString", "simpleArray", "incompleteArray", "clueDos"] as $method) {
+function bench(BenchCase $benchCase, array $methods) {
+    foreach ($methods as $method) {
         printf("%s: %f @ %s\n", get_class($benchCase->parser), benchSingle($benchCase, $method), $method);
     }
 }
@@ -35,6 +37,12 @@ abstract class BenchCase {
     public function simpleArray() {
         for ($i = 0; $i < 1000000; $i++) {
             $this->push("*2\r\n$5\r\nHello\r\n:123456789\r\n");
+        }
+    }
+
+    public function largerArray() {
+        for ($i = 0; $i < 100000; $i++) {
+            $this->push("*100\r\n" . str_repeat("$5\r\nHello\r\n", 100));
         }
     }
 
@@ -71,20 +79,44 @@ abstract class BenchCase {
     }
 }
 
-bench(new class(new IterativeRespParser(function () {})) extends BenchCase {
-    protected function push($data) {
-        $this->parser->push($data);
-    }
+$allMethods = ["simpleString", "bulkString", "simpleArray", "largerArray", "incompleteArray", /* "clueDos" */];
+
+$argv[1] = $argv[1] ?? implode(",", $allMethods);
+
+$methods = array_filter($allMethods, function ($method) use ($argv) {
+    return in_array($method, array_map("trim", explode(",", $argv[1])));
 });
 
-bench(new class(new RecursiveRespParser(function () {})) extends BenchCase {
+bench(new class(new IterativeRespParser(function () {
+})) extends BenchCase {
     protected function push($data) {
         $this->parser->push($data);
     }
-});
+}, $methods);
+
+bench(new class(new IterativeGeneratorRespParser(function () {
+})) extends BenchCase {
+    protected function push($data) {
+        $this->parser->push($data);
+    }
+}, $methods);
+
+bench(new class(new RecursiveGeneratorRespParser(function () {
+})) extends BenchCase {
+    protected function push($data) {
+        $this->parser->push($data);
+    }
+}, $methods);
+
+bench(new class(new RecursiveGeneratorRespParserWithoutInlining(function () {
+})) extends BenchCase {
+    protected function push($data) {
+        $this->parser->push($data);
+    }
+}, $methods);
 
 bench(new class(new ResponseParser) extends BenchCase {
     protected function push($data) {
         $this->parser->pushIncoming($data);
     }
-});
+}, $methods);
